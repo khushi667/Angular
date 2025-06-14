9d3479e5552a466ad674560a53108031e044ea88
khushi667 <khushi.mis03@gmail.com>
2025-06-14 17:30:38 +0530

    setup initial structure for custom component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [LoginComponent, SignupComponent],
+  imports: [LoginComponent, SignupComponent, ProfileComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })

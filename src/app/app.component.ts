52723564be9e9e8f2dd2df496c4d21afe02b274c
khushi667 <khushi.mis03@gmail.com>
2025-06-15 19:39:43 +0530

    learn show,hide,toggle box using control statment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,90 +12,20 @@ import { ProfileComponent } from './profile/profile.component';
   
 })
 export class AppComponent {
-  name=''
-  displayName=''
-  email=''
-  getName(event:Event){
-    this.name=(event.target as HTMLInputElement).value
+   display = true;
+    x=20
+    toggleDiv=true;
+
+    hide(){
+      this.display=false
+    }
+    show(){
+      this.display=true
+    }
+    toggle(){
+      this.display=!this.display
+    }
+    toggleTwo(){
+      this.toggleDiv=!this.toggleDiv;
+    }
   }
-  showName(){
-    this.displayName=this.name;
-  }
-  setName(){
-    this.name="prachi";
-  }
-  
-  getEmail(val:string){
-    console.log(val)
-    this.email=val
-  }
-  setEmail(){
-    this.email="default@test.com"
-  }
-  }
-
-
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  //    handleClickEvent(){
-  //   console.log("Function Called!!!")
-  //   this.otherFunction()    // we can't call otherFunction() like this coz the function is inside the class so we have to use this keyword
-  // }
-
-  // otherFunction(){
-  //   console.log("Other Function !!!")
-  // }
-
-  // name="Khushi"
-  // data:string | number = "Hello"
-  // other:any = true
-
-  // updateName(){
-  //   this.name="prachi"
-  //   this.data=123
-  //   this.other='om'
-  //   this.other=30
-  //   this.other={}
-  // }
-
-  //   updateVar(){
-  //     let x= 30
-  //     console.log(x)
-  //     this.sum(20,30)
-  //   }
-
-  //   sum(a:number,b:number){
-  //     console.log(a+b)
-
-  //   }
-
-  //    // title = 'My Angular App';
-  // // name = 'Khushi';
-  // // x = 5;
-  // // y = 10;
-  // // user1 = 'admin';
-  // // user2 = 'admin';
-
- 
-
-
-

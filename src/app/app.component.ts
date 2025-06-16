a3210afa834b4dde559c5dc4958e69d263ffe91d
khushi667 <khushi.mis03@gmail.com>
2025-06-16 15:09:07 +0530

    learn more about signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, effect, signal } from '@angular/core';
+import { Component, computed, effect, Signal, signal, WritableSignal } from '@angular/core';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { ProfileComponent } from './profile/profile.component';
@@ -12,21 +12,10 @@ import { ProfileComponent } from './profile/profile.component';
   
 })
 export class AppComponent {
-  count=signal(10);
-  x=20;
-  constructor(){
-    effect(()=>{
-      console.log(this.count())
-    })
-  }
-
-  updateValue(val:string){
-    // this.count.set(this.count()+1)
-    // this.x=this.x+1;
-    if(val=='inc'){
-      this.count.set(this.count()+1)
-    }else{
-      this.count.set(this.count()-1)
-    }
+  data: WritableSignal<number | string> =signal(10);
+  count :Signal<number> = computed(()=>200)
+  updateSignal(){
+    this.data.set('hello')
+    // this.count.set(20)  // we can't update computed signal 
   }
   }

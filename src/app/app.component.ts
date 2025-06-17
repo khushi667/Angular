2ef3fb8b2005bb38a8a41222c31b5ffd2bdf2f19
khushi667 <khushi.mis03@gmail.com>
2025-06-17 10:27:41 +0530

    learn two way binding using ngModule & event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,17 +3,21 @@ import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { ProfileComponent } from './profile/profile.component';
 import { timer } from 'rxjs';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [LoginComponent, SignupComponent, ProfileComponent],
+  imports: [LoginComponent, SignupComponent, ProfileComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
   
 })
 export class AppComponent {
-  users=['khushi','prachi','om','riya','priya','ekta','komal','roshni','sanu']
+  name=""
 
-  // users=[]
+  changeName(event:Event){
+    const val = (event.target as HTMLInputElement).value
+    this.name=val
+  }
   }
